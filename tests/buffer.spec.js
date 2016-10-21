8461474623cdeefc3705f0bd8b0f3bc8a60e8a0d
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-10-21 11:48:36 +0300

    100% coverage test

--- tests/buffer.spec.js
+++ tests/buffer.spec.js
@@ -4,14 +4,15 @@ var expect = require('chai').expect;
 
 
 describe('Testing Buffer object', function() {
+  var term = new Vector(20,30);
+  var cursor = new Vector(0,0);
+  var data = 'hello\nworld';
+
   it ('should be exist', function() {
     expect(Buffer).not.to.be.an('undefined');
   });
 
   it('should be created without errors', function() {
-    var term = new Vector(20,30);
-    var cursor = new Vector(0,0);
-    var data = 'hello\nworld';
 
     expect(term).to.be.an('object');
     expect(cursor).to.be.an('object');
@@ -21,4 +22,27 @@ describe('Testing Buffer object', function() {
 
     expect(buffer).to.be.an('object');
   });
+
+  it('sould buffer be filled externally', function() {
+    var buffer = new Buffer(term, cursor);
+
+    expect(buffer).to.be.an('object');
+
+    buffer.fillBuffer(data);
+
+    expect(buffer.buffer).to.be.an('array');
+    expect(buffer.buffer.length).to.be.equal(2);
+  });
+
+  it('sould render string', function() {
+    var buffer = new Buffer(term,cursor, data);
+
+    expect(buffer).to.be.an('object');
+
+    expect(buffer.render()).to.be.a('string');
+    expect(buffer.render()).to.contain('hello');
+  });
+
 });
+
+
