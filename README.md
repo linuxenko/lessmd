36ea6caa02579eb56fc03a3d691f7349e10b4068
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-10-23 16:46:05 +0300

    ----

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # lessmd
 
 [![Build Status](https://travis-ci.org/linuxenko/lessmd.svg?branch=master)](https://travis-ci.org/linuxenko/lessmd) [![Dependency Status](https://dependencyci.com/github/linuxenko/lessmd/badge)](https://dependencyci.com/github/linuxenko/lessmd) [![Coverage Status](https://coveralls.io/repos/github/linuxenko/lessmd/badge.svg)](https://coveralls.io/github/linuxenko/lessmd)
+
+
---
+++ dot-lessmd-sample.js
@@ -0,0 +1,23 @@
+module.exports = {
+//  colors : {
+//    text : ,
+//    lang : ,
+//    heading : ,
+//    code : ,
+//    quote : ,
+//    em : ,
+//    codespan : ,
+//    strong : ,
+//    html : ,
+//    del : ,
+//    link : ,
+//    hr : ,
+//    listitem :,
+//  },
+//  theme : {
+//    text : '\033[1;37;49m',
+//    strong : '\033[35;49m'
+//  },
+//  headerfn : function() { return 'header'; },
+//  footerfn : function() { return 'footer'; }
+};
